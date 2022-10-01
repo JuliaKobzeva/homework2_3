f0bef6af52b6505bd6c904c2b3747a454f630dce
Юля Кобзева <selemina@mail.ru>
2022-10-01 15:45:16 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car ladaGrande = new Car ("Lada", "Grande", 1.7, "желтый", 2015, "Россия","автомат", "седан", "н500оа678", 4, 200,"летняя", null, null);
+        Car audiA8 = new Car ("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "автомат", "седан", "р770ка860", 4, 200,"зимняя", null, null);
+        Car bmwZ8 = new Car ("BMW", "Z8", 3.0, "черный", 2021, "Германия", "автомат", "седан", "у539зв050", 4, 200,"летняя", null, null);
+        Car kiaSportage = new Car ("Kia", "Sportage 4 поколение", 2.4, "красный", 2018, "Южная Корея", "автомат", "седан", "с4890цв090", 4, 200,"зимняя", null, null);
+        Car hyundaiAvante = new Car ("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея", "автомат", "седан", "г777ду878", 4, 200,"летняя", null, null);
+
+
+        System.out.println(ladaGrande);
+        System.out.println(audiA8);
+        System.out.println(bmwZ8);
+        System.out.println(kiaSportage);
+        System.out.println(hyundaiAvante);
     }
 }
---
+++ src/Transport.java
@@ -0,0 +1,73 @@
+public class Transport {
+    private String brand;
+    private String model;
+    private int productionYear;
+    private String productionCountry;
+    private String color;
+    private int maxSpeed;
+
+    public Transport(String brand, String model, int productionYear, String productionCountry, String color, int maxSpeed) {
+        this.brand = brand;
+        this.model = model;
+
+        if(productionYear < 0){
+            this.productionYear = Math.abs(productionYear);
+        }else{
+            this.productionYear = productionYear;
+        }
+
+        this.productionCountry = productionCountry;
+
+        if(color == null || color.isBlank() || color.isEmpty()){
+            this.color = "белый";
+        }else{
+            this.color = color;
+        }
+
+        if(maxSpeed < 0){
+            this.maxSpeed = Math.abs(maxSpeed);
+        }else{
+            this.maxSpeed = maxSpeed;
+        }
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        if(color == null || color.isBlank() || color.isEmpty()){
+            this.color = "белый";
+        }else{
+            this.color = color;
+        }
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        if(maxSpeed < 0){
+            this.maxSpeed = Math.abs(maxSpeed);
+        }else{
+            this.maxSpeed = maxSpeed;
+        }
+    }
+}
